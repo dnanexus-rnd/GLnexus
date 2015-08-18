bea06e57d0c2e212a6e5c5ad04a1ade1c0dcc159
yifei-men <ymen@dnanexus.com>
2015-08-18 21:27:26 +0000

    Auto-updating Doxygen developer documentation

--- search/all_5.js
+++ search/all_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['import_5fgvcf',['import_gvcf',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a7a7e7d8638c30a2600b15829cb6cf8d6',1,'GLnexus::BCFKeyValueData']]],
   ['initializedb',['InitializeDB',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#aa62ac1393885a3fdfd1b7514697bba23',1,'GLnexus::BCFKeyValueData']]],
-  ['iterator',['Iterator',['../class_g_lnexus_1_1_rocks_key_value_1_1_iterator.html',1,'GLnexus::RocksKeyValue']]],
   ['iterator',['Iterator',['../class_g_lnexus_1_1_key_value_1_1_iterator.html',1,'GLnexus::KeyValue']]],
+  ['iterator',['Iterator',['../class_g_lnexus_1_1_rocks_key_value_1_1_iterator.html',1,'GLnexus::RocksKeyValue']]],
   ['iterator',['iterator',['../class_g_lnexus_1_1_key_value_1_1_reader.html#aacd95b4bcd33428f8854bf6f9f005043',1,'GLnexus::KeyValue::Reader::iterator(CollectionHandle coll, std::unique_ptr&lt; Iterator &gt; &amp;it) const =0'],['../class_g_lnexus_1_1_key_value_1_1_reader.html#a1d982caa2094bb39e82c18ee4104216b',1,'GLnexus::KeyValue::Reader::iterator(CollectionHandle coll, const std::string &amp;key, std::unique_ptr&lt; Iterator &gt; &amp;it) const =0'],['../class_g_lnexus_1_1_key_value_1_1_d_b.html#a0c9224f785f58ac0b5d7d4f7eb0dde36',1,'GLnexus::KeyValue::DB::iterator(CollectionHandle coll, std::unique_ptr&lt; Iterator &gt; &amp;it) const override'],['../class_g_lnexus_1_1_key_value_1_1_d_b.html#aa8d8fa5ed907cdb0301440b7aa84f32b',1,'GLnexus::KeyValue::DB::iterator(CollectionHandle coll, const std::string &amp;key, std::unique_ptr&lt; Iterator &gt; &amp;it) const override']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['range',['range',['../struct_g_lnexus_1_1range.html',1,'GLnexus']]],
-  ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]],
-  ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]]
+  ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
+  ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]]
 ];
