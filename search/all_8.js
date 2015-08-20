3f312dfbdecb280f714b32c793199f7eebe3fd68
yifei-men <ymen@dnanexus.com>
2015-08-20 21:17:25 +0000

    Auto-updating Doxygen developer documentation

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['range',['range',['../struct_g_lnexus_1_1range.html',1,'GLnexus']]],
+  ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]]
 ];
---
+++ search/functions_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['write_5fheader',['write_header',['../class_g_lnexus_1_1_b_c_f_writer.html#a6dada30c7d7a6e5a47f94f2f1e09078e',1,'GLnexus::BCFWriter']]]
+];
