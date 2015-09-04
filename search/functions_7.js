bbe843f0a83ea460e1877091a5a2781218296115
yifei-men <ymen@dnanexus.com>
2015-09-04 05:05:24 +0000

    Auto-updating Doxygen developer documentation

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['range',['range',['../struct_g_lnexus_1_1range.html#aeb8e939494ae3abf0de39d5630697467',1,'GLnexus::range']]],
   ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader']]]
 ];
