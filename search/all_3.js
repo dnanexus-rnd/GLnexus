9b2d5fdee0942550a14ab1cf04467997cbdf0672
yifei-men <ymen@dnanexus.com>
2015-09-09 17:55:00 +0000

    Auto-updating Doxygen developer documentation

--- search/all_3.js
+++ search/all_3.js
@@ -4,8 +4,8 @@ var searchData=
   ['datacache',['DataCache',['../class_g_lnexus_1_1_data_cache.html',1,'GLnexus']]],
   ['dataset_5fbcf',['dataset_bcf',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#ac4e4b179b5d20f9b74710e56711b89b9',1,'GLnexus::BCFKeyValueData::dataset_bcf()'],['../class_g_lnexus_1_1_data.html#a90e5d9afc91d55962a73c2d843c9f264',1,'GLnexus::Data::dataset_bcf()'],['../class_g_lnexus_1_1_data_cache.html#ab8ff3f0a566354ed78afd137e803c16b',1,'GLnexus::DataCache::dataset_bcf(const std::string &amp;dataset, const bcf_hdr_t *hdr, const range &amp;pos, std::vector&lt; std::shared_ptr&lt; bcf1_t &gt; &gt; &amp;records) const override'],['../class_g_lnexus_1_1_data_cache.html#ae49d24a27100b8e215e648b0001d53e0',1,'GLnexus::DataCache::dataset_bcf(const std::string &amp;dataset, const range &amp;pos, std::shared_ptr&lt; const bcf_hdr_t &gt; &amp;hdr, std::vector&lt; std::shared_ptr&lt; bcf1_t &gt; &gt; &amp;records) const ']]],
   ['dataset_5fbcf_5fheader',['dataset_bcf_header',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a88aabfb7f89cc681f2cb7df345412612',1,'GLnexus::BCFKeyValueData::dataset_bcf_header()'],['../class_g_lnexus_1_1_data.html#a26f28298e9fa5b5f5f865c1941ed9c4a',1,'GLnexus::Data::dataset_bcf_header()'],['../class_g_lnexus_1_1_data_cache.html#a98ee0338c0aa09b57fd001bd51bd9b57',1,'GLnexus::DataCache::dataset_bcf_header()']]],
-  ['db',['DB',['../class_g_lnexus_1_1_rocks_key_value_1_1_d_b.html',1,'GLnexus::RocksKeyValue']]],
   ['db',['DB',['../class_g_lnexus_1_1_key_value_1_1_d_b.html',1,'GLnexus::KeyValue']]],
+  ['db',['DB',['../class_g_lnexus_1_1_rocks_key_value_1_1_d_b.html',1,'GLnexus::RocksKeyValue']]],
   ['discover_5falleles',['discover_alleles',['../class_g_lnexus_1_1_service.html#ad25023a708863e26466c15cc999ebd22',1,'GLnexus::Service']]],
   ['discovered_5fallele_5finfo',['discovered_allele_info',['../struct_g_lnexus_1_1discovered__allele__info.html',1,'GLnexus']]]
 ];
