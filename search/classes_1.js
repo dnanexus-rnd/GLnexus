4f4e9928b7c95e617871523070ba2fa174eb78a6
yifei-men <ymen@dnanexus.com>
2015-09-09 06:05:10 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,10 +1,8 @@
 var searchData=
 [
-  ['bcfbucketrange',['BCFBucketRange',['../class_g_lnexus_1_1_b_c_f_bucket_range.html',1,'GLnexus']]],
   ['bcfkeyvaluedata',['BCFKeyValueData',['../class_g_lnexus_1_1_b_c_f_key_value_data.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
   ['body',['body',['../struct_g_lnexus_1_1_data_cache_1_1body.html',1,'GLnexus::DataCache']]],
-  ['body',['body',['../struct_g_lnexus_1_1_b_c_f_key_value_data_1_1body.html',1,'GLnexus::BCFKeyValueData']]],
-  ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
+  ['body',['body',['../struct_g_lnexus_1_1_b_c_f_key_value_data_1_1body.html',1,'GLnexus::BCFKeyValueData']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['import_5fgvcf',['import_gvcf',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a7a7e7d8638c30a2600b15829cb6cf8d6',1,'GLnexus::BCFKeyValueData']]],
-  ['initializedb',['InitializeDB',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#ae1d886374b8929b63d36137a433a8427',1,'GLnexus::BCFKeyValueData']]],
+  ['import_5fgvcf',['import_gvcf',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#aa81d5e303bd9f85ff72ead85874c9054',1,'GLnexus::BCFKeyValueData']]],
+  ['initializedb',['InitializeDB',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#aa62ac1393885a3fdfd1b7514697bba23',1,'GLnexus::BCFKeyValueData']]],
   ['iterator',['iterator',['../class_g_lnexus_1_1_key_value_1_1_reader.html#aacd95b4bcd33428f8854bf6f9f005043',1,'GLnexus::KeyValue::Reader::iterator(CollectionHandle coll, std::unique_ptr&lt; Iterator &gt; &amp;it) const =0'],['../class_g_lnexus_1_1_key_value_1_1_reader.html#a1d982caa2094bb39e82c18ee4104216b',1,'GLnexus::KeyValue::Reader::iterator(CollectionHandle coll, const std::string &amp;key, std::unique_ptr&lt; Iterator &gt; &amp;it) const =0'],['../class_g_lnexus_1_1_key_value_1_1_d_b.html#a0c9224f785f58ac0b5d7d4f7eb0dde36',1,'GLnexus::KeyValue::DB::iterator(CollectionHandle coll, std::unique_ptr&lt; Iterator &gt; &amp;it) const override'],['../class_g_lnexus_1_1_key_value_1_1_d_b.html#aa8d8fa5ed907cdb0301440b7aa84f32b',1,'GLnexus::KeyValue::DB::iterator(CollectionHandle coll, const std::string &amp;key, std::unique_ptr&lt; Iterator &gt; &amp;it) const override']]]
 ];
