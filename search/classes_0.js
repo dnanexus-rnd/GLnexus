1f306b8a64d1957f2188a776e618d32964b3bc3b
yifei-men <ymen@dnanexus.com>
2015-09-18 22:28:57 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['activemetadata',['ActiveMetadata',['../struct_g_lnexus_1_1_active_metadata.html',1,'GLnexus']]],
   ['allele',['allele',['../struct_g_lnexus_1_1allele.html',1,'GLnexus']]],
   ['alleledepthhelper',['AlleleDepthHelper',['../class_g_lnexus_1_1_allele_depth_helper.html',1,'GLnexus']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -5,7 +5,7 @@ var searchData=
   ['bcfkeyvaluedata',['BCFKeyValueData',['../class_g_lnexus_1_1_b_c_f_key_value_data.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
-  ['body',['body',['../struct_g_lnexus_1_1_b_c_f_key_value_data_1_1body.html',1,'GLnexus::BCFKeyValueData']]],
   ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
+  ['body',['body',['../struct_g_lnexus_1_1_b_c_f_key_value_data_1_1body.html',1,'GLnexus::BCFKeyValueData']]],
   ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
 ];
