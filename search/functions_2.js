d1287cfc6de68de1c1cc5aefafe44b6f7cfd935f
yifei-men <ymen@dnanexus.com>
2015-09-18 23:23:10 +0000

    Auto-updating Doxygen developer documentation

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['dataset_5fheader',['dataset_header',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a0e15218d1d078bf6edf2af065e55573d',1,'GLnexus::BCFKeyValueData::dataset_header()'],['../class_g_lnexus_1_1_b_c_f_data.html#a5eabf92aa1b3948d95fc0f01ceb445f0',1,'GLnexus::BCFData::dataset_header()']]],
-  ['dataset_5frange',['dataset_range',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a83a8f793b1b9ce07412ff90c1a63debc',1,'GLnexus::BCFKeyValueData::dataset_range()'],['../class_g_lnexus_1_1_b_c_f_data.html#a0879d81ed86d2fa5173e30a95c5c58a4',1,'GLnexus::BCFData::dataset_range()']]],
-  ['dataset_5frange_5fand_5fheader',['dataset_range_and_header',['../class_g_lnexus_1_1_b_c_f_data.html#a5976e3fe73b51c5f5a5194b36966e47f',1,'GLnexus::BCFData']]],
+  ['dataset_5frange',['dataset_range',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#aa645df4f9a2b63023c8586775c13a6b7',1,'GLnexus::BCFKeyValueData::dataset_range()'],['../class_g_lnexus_1_1_b_c_f_data.html#a13ebb72e7eb651521ec909bc99359771',1,'GLnexus::BCFData::dataset_range()']]],
+  ['dataset_5frange_5fand_5fheader',['dataset_range_and_header',['../class_g_lnexus_1_1_b_c_f_data.html#a0e4bf380734b1886c17dd1db08fa3c2a',1,'GLnexus::BCFData']]],
   ['discover_5falleles',['discover_alleles',['../class_g_lnexus_1_1_service.html#ad25023a708863e26466c15cc999ebd22',1,'GLnexus::Service']]]
 ];
