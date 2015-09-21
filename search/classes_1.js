ddb9755d5c3194e8f33467dd2b46a8a532d183d3
yifei-men <ymen@dnanexus.com>
2015-09-21 19:29:59 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_1.js
+++ search/classes_1.js
@@ -3,9 +3,9 @@ var searchData=
   ['bcfbucketrange',['BCFBucketRange',['../class_g_lnexus_1_1_b_c_f_bucket_range.html',1,'GLnexus']]],
   ['bcfdata',['BCFData',['../class_g_lnexus_1_1_b_c_f_data.html',1,'GLnexus']]],
   ['bcfkeyvaluedata',['BCFKeyValueData',['../class_g_lnexus_1_1_b_c_f_key_value_data.html',1,'GLnexus']]],
+  ['bcfkeyvaluedata_5fbody',['BCFKeyValueData_body',['../struct_g_lnexus_1_1_b_c_f_key_value_data__body.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
   ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
-  ['body',['body',['../struct_g_lnexus_1_1_b_c_f_key_value_data_1_1body.html',1,'GLnexus::BCFKeyValueData']]],
   ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
 ];
