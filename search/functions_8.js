aeca0fa527566e3d88571e8cbd269fd18bff9765
yifei-men <ymen@dnanexus.com>
2015-09-29 22:57:17 +0000

    Auto-updating Doxygen developer documentation

--- search/functions_8.js
+++ search/functions_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['sample_5fdataset',['sample_dataset',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#af0eebb65fd9c969f2c4746506643f022',1,'GLnexus::BCFKeyValueData::sample_dataset()'],['../class_g_lnexus_1_1_metadata.html#a4e02401b7c72fac69a63992fbb888ac7',1,'GLnexus::Metadata::sample_dataset()'],['../class_g_lnexus_1_1_metadata_cache.html#a8b1c9724ff7401469410766435e5cf09',1,'GLnexus::MetadataCache::sample_dataset()']]],
-  ['sampleset_5fsamples',['sampleset_samples',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a31f058179be1f19bc410c7a9dfa2833f',1,'GLnexus::BCFKeyValueData::sampleset_samples()'],['../class_g_lnexus_1_1_metadata.html#a1ae700d4d003c3b26a8bc5c1dce558f2',1,'GLnexus::Metadata::sampleset_samples()'],['../class_g_lnexus_1_1_metadata_cache.html#af2e4e69e17c7287a18b5f78a5037ced4',1,'GLnexus::MetadataCache::sampleset_samples()']]],
-  ['status',['Status',['../class_g_lnexus_1_1_status.html#a69dfdcf0c8dd43a077df43fa34478e71',1,'GLnexus::Status::Status(StatusCode code=StatusCode::OK, const char *msg=nullptr) noexcept'],['../class_g_lnexus_1_1_status.html#a579b468687fe1aa8009741282f31128e',1,'GLnexus::Status::Status(StatusCode code, const char *msg, const std::string &amp;noun)']]]
+  ['range',['range',['../struct_g_lnexus_1_1range.html#aeb8e939494ae3abf0de39d5630697467',1,'GLnexus::range']]],
+  ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader']]]
 ];
