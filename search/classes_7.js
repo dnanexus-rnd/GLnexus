b6ff2137ed3a19e9225658d0a4a4a978530f8400
yifei-men <ymen@dnanexus.com>
2015-10-01 18:59:42 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['one_5fcall',['one_call',['../struct_g_lnexus_1_1one__call.html',1,'GLnexus']]],
   ['orig_5fcall',['orig_call',['../struct_g_lnexus_1_1_loss_tracker_1_1orig__call.html',1,'GLnexus::LossTracker']]]
 ];
