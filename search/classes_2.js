a0915fe21dfffe5e803ee4a5430b4312daed9d90
yifei-men <ymen@dnanexus.com>
2015-10-02 17:38:59 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['db',['DB',['../class_g_lnexus_1_1_rocks_key_value_1_1_d_b.html',1,'GLnexus::RocksKeyValue']]],
   ['db',['DB',['../class_g_lnexus_1_1_key_value_1_1_d_b.html',1,'GLnexus::KeyValue']]],
+  ['db',['DB',['../class_g_lnexus_1_1_rocks_key_value_1_1_d_b.html',1,'GLnexus::RocksKeyValue']]],
   ['discovered_5fallele_5finfo',['discovered_allele_info',['../struct_g_lnexus_1_1discovered__allele__info.html',1,'GLnexus']]]
 ];
