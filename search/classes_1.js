c4fcccecbd35cbdc2db2f2f9a07acef45af4866a
yifei-men <ymen@dnanexus.com>
2015-10-09 18:39:14 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_1.js
+++ search/classes_1.js
@@ -4,6 +4,7 @@ var searchData=
   ['bcfbucketcache_5fbody',['BCFBucketCache_body',['../struct_g_lnexus_1_1_b_c_f_bucket_cache__body.html',1,'GLnexus']]],
   ['bcfbucketrange',['BCFBucketRange',['../class_g_lnexus_1_1_b_c_f_bucket_range.html',1,'GLnexus']]],
   ['bcfdata',['BCFData',['../class_g_lnexus_1_1_b_c_f_data.html',1,'GLnexus']]],
+  ['bcffilesink',['BCFFileSink',['../class_g_lnexus_1_1_b_c_f_file_sink.html',1,'GLnexus']]],
   ['bcfkeyvaluedata',['BCFKeyValueData',['../class_g_lnexus_1_1_b_c_f_key_value_data.html',1,'GLnexus']]],
   ['bcfkeyvaluedata_5fbody',['BCFKeyValueData_body',['../struct_g_lnexus_1_1_b_c_f_key_value_data__body.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
