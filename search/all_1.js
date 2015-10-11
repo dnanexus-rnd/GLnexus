d2309a3852910296a9ff40003f46939d8fd50261
yifei-men <ymen@dnanexus.com>
2015-10-11 18:35:26 +0000

    Auto-updating Doxygen developer documentation

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['bcfbucketcache',['BCFBucketCache',['../class_g_lnexus_1_1_b_c_f_bucket_cache.html',1,'GLnexus']]],
-  ['bcfbucketcache_5fbody',['BCFBucketCache_body',['../struct_g_lnexus_1_1_b_c_f_bucket_cache__body.html',1,'GLnexus']]],
   ['bcfbucketrange',['BCFBucketRange',['../class_g_lnexus_1_1_b_c_f_bucket_range.html',1,'GLnexus']]],
   ['bcfdata',['BCFData',['../class_g_lnexus_1_1_b_c_f_data.html',1,'GLnexus']]],
   ['bcffilesink',['BCFFileSink',['../class_g_lnexus_1_1_b_c_f_file_sink.html',1,'GLnexus']]],
@@ -10,7 +8,7 @@ var searchData=
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
   ['begin_5fwrites',['begin_writes',['../class_g_lnexus_1_1_key_value_1_1_d_b.html#a7132dd1e35e514b593da7da5b02c569d',1,'GLnexus::KeyValue::DB']]],
-  ['body',['body',['../struct_g_lnexus_1_1_service_1_1body.html',1,'GLnexus::Service']]],
   ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
+  ['body',['body',['../struct_g_lnexus_1_1_service_1_1body.html',1,'GLnexus::Service']]],
   ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['bcfbucketcache',['BCFBucketCache',['../class_g_lnexus_1_1_b_c_f_bucket_cache.html',1,'GLnexus']]],
-  ['bcfbucketcache_5fbody',['BCFBucketCache_body',['../struct_g_lnexus_1_1_b_c_f_bucket_cache__body.html',1,'GLnexus']]],
   ['bcfbucketrange',['BCFBucketRange',['../class_g_lnexus_1_1_b_c_f_bucket_range.html',1,'GLnexus']]],
   ['bcfdata',['BCFData',['../class_g_lnexus_1_1_b_c_f_data.html',1,'GLnexus']]],
   ['bcffilesink',['BCFFileSink',['../class_g_lnexus_1_1_b_c_f_file_sink.html',1,'GLnexus']]],
@@ -9,7 +7,7 @@ var searchData=
   ['bcfkeyvaluedata_5fbody',['BCFKeyValueData_body',['../struct_g_lnexus_1_1_b_c_f_key_value_data__body.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
-  ['body',['body',['../struct_g_lnexus_1_1_service_1_1body.html',1,'GLnexus::Service']]],
   ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
+  ['body',['body',['../struct_g_lnexus_1_1_service_1_1body.html',1,'GLnexus::Service']]],
   ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
 ];
