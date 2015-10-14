3137e497e5440a4a430b8ff484ed41d8a53a0257
yifei-men <ymen@dnanexus.com>
2015-10-14 14:57:22 +0000

    Auto-updating Doxygen developer documentation

--- search/all_b.js
+++ search/all_b.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['range',['range',['../struct_g_lnexus_1_1range.html',1,'GLnexus']]],
   ['range',['range',['../struct_g_lnexus_1_1range.html#aeb8e939494ae3abf0de39d5630697467',1,'GLnexus::range']]],
+  ['rangebcfiterator',['RangeBCFIterator',['../class_g_lnexus_1_1_range_b_c_f_iterator.html',1,'GLnexus']]],
   ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader']]],
-  ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]],
+  ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
   ['ref_5fdp_5fformat',['ref_dp_format',['../struct_g_lnexus_1_1genotyper__config.html#a71cc0d0a145034fdd893a28e9cae240a',1,'GLnexus::genotyper_config']]],
   ['ref_5fsymbolic_5fallele',['ref_symbolic_allele',['../struct_g_lnexus_1_1genotyper__config.html#ae5105c6c4c3e1417f8f1b41324931560',1,'GLnexus::genotyper_config']]],
   ['required_5fdp',['required_dp',['../struct_g_lnexus_1_1genotyper__config.html#a99811beeefbe17ed9b9f93b1ba727188',1,'GLnexus::genotyper_config']]],
--- search/all_c.js
+++ search/all_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['sample_5fdataset',['sample_dataset',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#af0eebb65fd9c969f2c4746506643f022',1,'GLnexus::BCFKeyValueData::sample_dataset()'],['../class_g_lnexus_1_1_metadata.html#a4e02401b7c72fac69a63992fbb888ac7',1,'GLnexus::Metadata::sample_dataset()'],['../class_g_lnexus_1_1_metadata_cache.html#a8b1c9724ff7401469410766435e5cf09',1,'GLnexus::MetadataCache::sample_dataset()']]],
+  ['sampleset_5frange',['sampleset_range',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#aa1d2826d55178b52f0c2282e45fd703e',1,'GLnexus::BCFKeyValueData::sampleset_range()'],['../class_g_lnexus_1_1_b_c_f_data.html#a4f6ef0f8d4c1ddb0da9faa3924949108',1,'GLnexus::BCFData::sampleset_range()']]],
   ['sampleset_5fsamples',['sampleset_samples',['../class_g_lnexus_1_1_b_c_f_key_value_data.html#a31f058179be1f19bc410c7a9dfa2833f',1,'GLnexus::BCFKeyValueData::sampleset_samples()'],['../class_g_lnexus_1_1_metadata.html#a1ae700d4d003c3b26a8bc5c1dce558f2',1,'GLnexus::Metadata::sampleset_samples()'],['../class_g_lnexus_1_1_metadata_cache.html#af2e4e69e17c7287a18b5f78a5037ced4',1,'GLnexus::MetadataCache::sampleset_samples()']]],
   ['service',['Service',['../class_g_lnexus_1_1_service.html',1,'GLnexus']]],
   ['statsrangequery',['StatsRangeQuery',['../struct_g_lnexus_1_1_stats_range_query.html',1,'GLnexus']]],
