a22419fc1024f57840497f914c876223b03b7053
yifei-men <ymen@dnanexus.com>
2015-11-02 16:06:09 +0000

    Auto-updating Doxygen developer documentation

--- search/all_1.js
+++ search/all_1.js
@@ -7,10 +7,9 @@ var searchData=
   ['bcfkeyvaluedata',['BCFKeyValueData',['../class_g_lnexus_1_1_b_c_f_key_value_data.html',1,'GLnexus']]],
   ['bcfkeyvaluedata_5fbody',['BCFKeyValueData_body',['../struct_g_lnexus_1_1_b_c_f_key_value_data__body.html',1,'GLnexus']]],
   ['bcfreader',['BCFReader',['../class_g_lnexus_1_1_b_c_f_reader.html',1,'GLnexus']]],
-  ['bcfscanner',['BCFScanner',['../class_g_lnexus_1_1_b_c_f_scanner.html',1,'GLnexus']]],
   ['bcfwriter',['BCFWriter',['../class_g_lnexus_1_1_b_c_f_writer.html',1,'GLnexus']]],
   ['begin_5fwrites',['begin_writes',['../class_g_lnexus_1_1_key_value_1_1_d_b.html#a7132dd1e35e514b593da7da5b02c569d',1,'GLnexus::KeyValue::DB']]],
-  ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
   ['body',['body',['../struct_g_lnexus_1_1_service_1_1body.html',1,'GLnexus::Service']]],
+  ['body',['body',['../struct_g_lnexus_1_1_metadata_cache_1_1body.html',1,'GLnexus::MetadataCache']]],
   ['bucketextent',['BucketExtent',['../class_g_lnexus_1_1_bucket_extent.html',1,'GLnexus']]]
 ];
--- search/all_b.js
+++ search/all_b.js
@@ -3,7 +3,7 @@ var searchData=
   ['range',['range',['../struct_g_lnexus_1_1range.html',1,'GLnexus']]],
   ['range',['range',['../struct_g_lnexus_1_1range.html#aeb8e939494ae3abf0de39d5630697467',1,'GLnexus::range']]],
   ['rangebcfiterator',['RangeBCFIterator',['../class_g_lnexus_1_1_range_b_c_f_iterator.html',1,'GLnexus']]],
-  ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader::read_header()'],['../class_g_lnexus_1_1_b_c_f_scanner.html#a815fa2a54ac7243e3d9098fe1b11ce29',1,'GLnexus::BCFScanner::read_header()']]],
+  ['read_5fheader',['read_header',['../class_g_lnexus_1_1_b_c_f_reader.html#a9d42d179c44b3277eaa8bc85b83e6f29',1,'GLnexus::BCFReader']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]],
   ['ref_5fdp_5fformat',['ref_dp_format',['../struct_g_lnexus_1_1genotyper__config.html#a71cc0d0a145034fdd893a28e9cae240a',1,'GLnexus::genotyper_config']]],
