a983bd6c3f15103ec578f2956b8c4d3a1d67eb64
yifei-men <ymen@dnanexus.com>
2015-11-16 04:36:14 +0000

    Auto-updating Doxygen developer documentation

--- search/classes_7.js
+++ search/classes_7.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['metadata',['Metadata',['../class_g_lnexus_1_1_metadata.html',1,'GLnexus']]],
-  ['metadatacache',['MetadataCache',['../class_g_lnexus_1_1_metadata_cache.html',1,'GLnexus']]],
-  ['minimized_5fallele_5finfo',['minimized_allele_info',['../struct_g_lnexus_1_1minimized__allele__info.html',1,'GLnexus']]]
+  ['metadatacache',['MetadataCache',['../class_g_lnexus_1_1_metadata_cache.html',1,'GLnexus']]]
 ];
