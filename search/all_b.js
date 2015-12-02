d620b753a0e96ee2e7bbd7db4cf3e825938a3ce1
yifei-men <ymen@dnanexus.com>
2015-12-02 19:05:25 +0000

    Auto-updating Doxygen developer documentation

--- search/all_b.js
+++ search/all_b.js
@@ -9,5 +9,7 @@ var searchData=
   ['ref_5fdp_5fformat',['ref_dp_format',['../struct_g_lnexus_1_1genotyper__config.html#a71cc0d0a145034fdd893a28e9cae240a',1,'GLnexus::genotyper_config']]],
   ['ref_5fsymbolic_5fallele',['ref_symbolic_allele',['../struct_g_lnexus_1_1genotyper__config.html#ae5105c6c4c3e1417f8f1b41324931560',1,'GLnexus::genotyper_config']]],
   ['required_5fdp',['required_dp',['../struct_g_lnexus_1_1genotyper__config.html#a99811beeefbe17ed9b9f93b1ba727188',1,'GLnexus::genotyper_config']]],
+  ['residuals',['Residuals',['../class_g_lnexus_1_1_residuals.html',1,'GLnexus']]],
+  ['residualsfile',['ResidualsFile',['../class_g_lnexus_1_1_residuals_file.html',1,'GLnexus']]],
   ['rnc',['RNC',['../struct_g_lnexus_1_1one__call.html#a06294bc9d3191bd4421d9a5407853364',1,'GLnexus::one_call']]]
 ];
--- search/classes_9.js
+++ search/classes_9.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['range',['range',['../struct_g_lnexus_1_1range.html',1,'GLnexus']]],
   ['rangebcfiterator',['RangeBCFIterator',['../class_g_lnexus_1_1_range_b_c_f_iterator.html',1,'GLnexus']]],
+  ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]],
   ['reader',['Reader',['../class_g_lnexus_1_1_rocks_key_value_1_1_reader.html',1,'GLnexus::RocksKeyValue']]],
-  ['reader',['Reader',['../class_g_lnexus_1_1_key_value_1_1_reader.html',1,'GLnexus::KeyValue']]]
+  ['residuals',['Residuals',['../class_g_lnexus_1_1_residuals.html',1,'GLnexus']]],
+  ['residualsfile',['ResidualsFile',['../class_g_lnexus_1_1_residuals_file.html',1,'GLnexus']]]
 ];
