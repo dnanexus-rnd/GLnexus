9ebd274bd498655fec8593780fbaf2b6cbda9d53
Ohad Rodeh <orodeh@dnanexus.com>
2016-09-20 15:46:14 -0700

    Version that compiles and de/serializes discovered-alleles with capnp

--- capnp/serialize/defs.capnp
+++ capnp/serialize/defs.capnp
@@ -4,12 +4,8 @@ using Cxx = import "/capnp/c++.capnp";
 $Cxx.namespace("GLnexus::capnp");
 
 struct TopAQ {
-    v @0 :List(UInt64);
-    addbuf @1 :List(UInt64);
-}
-
-struct ZygosityByGQ {
-    m @0 :List(List(UInt64));
+    v @0 :List(Int64);
+    addbuf @1 :List(Int64);
 }
 
 struct DiscoveredAlleleInfo {
@@ -18,6 +14,25 @@ struct DiscoveredAlleleInfo {
     # top_AQ statistics are used to adjudicate allele existence
     topAQ @1 :TopAQ;
 
-    # zygosity_by_GQ statsitics are used to estimate allele copy number
-    zGQ @2 :ZygosityByGQ;
+     # zygosity_by_GQ statistics are used to estimate allele copy number
+    zGQ0 @2 :List(UInt64);
+    zGQ1 @3 :List(UInt64);
+}
+
+struct Range {
+    rid @0 :Int64;
+    beg @1 :Int64;
+    end @2 :Int64;
+}
+
+struct AlleleInfoPair {
+    range @0 :Range;
+    dna @1 :Text;
+    dai @2 :DiscoveredAlleleInfo;
+}
+
+# This is really a mapping from allele to information.
+struct DiscoveredAlleles {
+    # aip = allele info pairs
+    aips @0 :List(AlleleInfoPair);
 }
--- src/capnp_serialize.cc
+++ src/capnp_serialize.cc
@@ -1,8 +1,8 @@
+#include <type_traits>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 #include <iostream>
@@ -15,46 +15,145 @@ using namespace std;
 namespace GLnexus {
 namespace capnp {
 
-// write discovered_alleles structure to a file-descriptor, with cap'n proto serialization
-Status write_discovered_alleles(int fd,
-                                const std::vector<std::pair<std::string,size_t> >& contigs,
-                                const discovered_alleles &dsals) {
-    return Status::Invalid("Not implemented yet");
-}
-
-// read discovered_alleles structure from a file-descriptor, as serialized by cap'n proto
-Status read_discovered_alleles(int fd,
-                               const std::vector<std::pair<std::string,size_t> >& contigs,
-                               discovered_alleles&) {
-    return Status::Invalid("Not implemented yet");
-}
+// We assume the chromosome ploidy is two in our serialization format.
+static_assert(zygosity_by_GQ::PLOIDY == 2, "PLOIDY needs to be two");
 
 Status discover_alleles_verify(const std::vector<std::pair<std::string,size_t> >& contigs,
                                const discovered_alleles &dsals) {
     Status s;
 
-    // create temporary file
-    std::FILE* tmpf = std::tmpfile();
-    int fd = fileno(tmpf);
+    string filename = "/tmp/test_discover_alleles";
 
-    // write to it
-    S(write_discovered_alleles(fd, contigs, dsals));
-
-    // read from it
-    discovered_alleles dsals2;
-    S(read_discovered_alleles(fd, contigs, dsals2));
-
-    // Close the file-descriptor, this will also delete the file
-    // once the program is closed.
-    int rc = close(fd);
-    if (rc != 0)
-        return Status::IOError("Could not close file-descriptor to temporary file");
-
-    // verify we get the same alleles back
-    if (dsals == dsals2) {
-        return Status::OK();
+    {
+        // Write to file
+        int fd = open(filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+        S(write_discovered_alleles(fd, contigs, dsals));
+        fsync(fd);
+        close(fd);
     }
+
+    {
+        // read from it
+        int fd = open(filename.c_str(), O_RDONLY);
+        discovered_alleles dsals2;
+        S(read_discovered_alleles(fd, contigs, dsals2));
+        close(fd);
+
+        // verify we get the same alleles back
+        if (dsals == dsals2) {
+            return Status::OK();
+        }
+    }
+
     return Status::Invalid("capnp serialization/deserialization of discovered alleles does not return original value");
 }
+
+// write discovered_alleles structure to a file-descriptor, with cap'n proto serialization
+Status write_discovered_alleles(int fd,
+                                const std::vector<std::pair<std::string,size_t> >& contigs,
+                                const discovered_alleles &dsals_i) {
+    ::capnp::MallocMessageBuilder message;
+    DiscoveredAlleles::Builder dsals = message.initRoot<DiscoveredAlleles>();
+
+    // serialize allele-info pairs
+    ::capnp::List<AlleleInfoPair>::Builder aips = dsals.initAips(dsals_i.size());
+
+    int cursor = 0;
+    for (auto const &kv : dsals_i) {
+        auto &key = kv.first;
+        auto &val = kv.second;
+
+        AlleleInfoPair::Builder al = aips[cursor];
+        Range::Builder range = al.initRange();
+        range.setRid(key.pos.rid);
+        range.setBeg(key.pos.beg);
+        range.setEnd(key.pos.end);
+        al.setDna(key.dna.c_str());
+
+        DiscoveredAlleleInfo::Builder dai = al.initDai();
+        dai.setIsRef(val.is_ref);
+
+        {
+            TopAQ::Builder topAQ = dai.initTopAQ();
+            ::capnp::List<int64_t>::Builder v = topAQ.initV(top_AQ::COUNT);
+            for (int k=0; k < top_AQ::COUNT; k++)
+                v.set(k, val.topAQ.V[k]);
+            topAQ.setV(v);
+
+            int nelem = val.topAQ.addbuf.size();
+            ::capnp::List<int64_t>::Builder addbuf = topAQ.initV(nelem);
+            for (int k=0; k < nelem; k++)
+                addbuf.set(k, val.topAQ.addbuf[k]);
+            topAQ.setAddbuf(addbuf);
+        }
+
+        {
+            ::capnp::List<uint64_t>::Builder zGQ0 = dai.initZGQ0(zygosity_by_GQ::GQ_BANDS);
+            for (int k=0; k < zygosity_by_GQ::GQ_BANDS; k++)
+                zGQ0.set(k, val.zGQ.M[k][0]);
+            dai.setZGQ0(zGQ0);
+
+            ::capnp::List<uint64_t>::Builder zGQ1 = dai.initZGQ1(zygosity_by_GQ::GQ_BANDS);
+            for (int k=0; k < zygosity_by_GQ::GQ_BANDS; k++)
+                zGQ0.set(k, val.zGQ.M[k][1]);
+            dai.setZGQ1(zGQ1);
+        }
+
+        cursor++;
+    }
+
+    writePackedMessageToFd(fd, message);
+    return Status::OK();
+}
+
+// read discovered_alleles structure from a file-descriptor, as serialized by cap'n proto
+Status read_discovered_alleles(int fd,
+                               const std::vector<std::pair<std::string,size_t> >& contigs,
+                               discovered_alleles &dsals) {
+    ::capnp::PackedFdMessageReader message(fd);
+    DiscoveredAlleles::Reader dsals_pk = message.getRoot<DiscoveredAlleles>();
+
+    for (AlleleInfoPair::Reader aip : dsals_pk.getAips()) {
+        // Unpack allele
+        Range::Reader range_pk = aip.getRange();
+        range r(range_pk.getRid(),
+                range_pk.getBeg(),
+                range_pk.getEnd());
+        string dna = aip.getDna();
+        allele alle(r, dna);
+
+        // Unpack allele-info
+        discovered_allele_info dai;
+        DiscoveredAlleleInfo::Reader dai_pk = aip.getDai();
+        dai.is_ref = dai_pk.getIsRef();
+
+        TopAQ::Reader topAQ_pk = dai_pk.getTopAQ();
+        ::capnp::List<int64_t>::Reader v_pk = topAQ_pk.getV();
+        for (int k=0; k < v_pk.size(); k++)  {
+            dai.topAQ.V[k] =  v_pk[k];
+        }
+        ::capnp::List<int64_t>::Reader addbuf_pk = topAQ_pk.getAddbuf();
+        for (int k=0; k < addbuf_pk.size(); k++) {
+            dai.topAQ.addbuf.push_back(addbuf_pk[k]);
+        }
+
+        // Unpack Zygosity information
+        ::capnp::List<uint64_t>::Reader zGQ0_pk = dai_pk.getZGQ0();
+        ::capnp::List<uint64_t>::Reader zGQ1_pk = dai_pk.getZGQ1();
+        if (zGQ0_pk.size() != zygosity_by_GQ::GQ_BANDS)
+            return Status::Invalid("Wrong number of GQ_BANDS", to_string(zGQ0_pk.size()));
+        if (zGQ1_pk.size() != zygosity_by_GQ::GQ_BANDS)
+            return Status::Invalid("Wrong number of GQ_BANDS", to_string(zGQ1_pk.size()));
+        for (int k=0; k < zygosity_by_GQ::GQ_BANDS; k++) {
+            dai.zGQ.M[k][0] = zGQ0_pk[k];
+            dai.zGQ.M[k][1] = zGQ1_pk[k];
+        }
+
+        dsals[alle] = dai;
+    }
+
+    return Status::OK();
+}
+
 
 }} // namespace GLnexus
