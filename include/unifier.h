69e6ae87e777e72f3fdd74a904cb89e703d8796f
Mike Lin <mlin@mlin.net>
2017-03-27 14:52:10 -0700

    unifier: reduce memory usage
    Change a couple of loops over (usually large) data structures of alleles to
    free the input incrementally during iteration, reducing peak memory usage.
    This makes several unifier functions have the side effect of clearing an
    input, slightly perilous but worth it since unifier memory usage is one of
    our scalability chokepoints.

--- include/unifier.h
+++ include/unifier.h
@@ -9,8 +9,13 @@ namespace GLnexus {
 
 /// Compute unified sites from all discovered alleles in some genomic region
 /// N = sample count (used to estimate allele frequencies)
+// Important I/O notes:
+// (1) The input discovered_alleles structure is cleared as a
+//     side-effect, to save memory since it can be quite large.
+// (2) ans is NOT cleared, only appended to.
 Status unified_sites(const unifier_config& cfg,
-                     unsigned N, const discovered_alleles& alleles,
+                     unsigned N,
+                     /* const */ discovered_alleles& alleles,
                      std::vector<unified_site>& ans);
 }
 
