d4c26aafa1171f59bfc4ad66b3fa821999c2ba50
Mike Lin <mlin@mlin.net>
2017-06-07 14:37:34 -0700

    zstd compression

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -e
 
-# PPAs
-sudo add-apt-repository -y ppa:nathan-renniewaldock/ppa # lz4
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test # gcc 4.9
+# Install gcc 5
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
-
-# The lz4 library requires a special repository
-sudo apt-get -qq install -y liblz4-dev
-
-# Install GCC 4.9
 sudo apt-get -qq install -y gcc-5 g++-5 binutils
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 \
                          --slave /usr/bin/g++ g++ /usr/bin/g++-5 \
@@ -16,3 +10,8 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 \
 
 # Install boost for use in yaml-cpp
 sudo apt-get -qq install -y libboost1.55-dev
+
+# Install zstd
+tar zxf <(curl -L https://github.com/facebook/zstd/archive/v1.2.0.tar.gz)
+make -C zstd-* -j8
+sudo make -C zstd-* install
