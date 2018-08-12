dnanexus-rnd/GLnexus

commit 9832344ce194f9bda1f971cc187ad178921a3b8a

command-line options for memory and thread budgets (#134)

src/RocksKeyValue.cc
@@ -81,8 +81,22 @@ static Status convertStatus(const rocksdb::Status &s)
     }
 }
 
+// Create RocksDB block cache to be shared among all collections in one database
+std::shared_ptr<rocksdb::Cache> NewBlockCache(OpenMode mode, size_t mem_budget) {
+    mem_budget = std::max(mem_budget, size_t(2<<30));
+    mem_budget = std::min(mem_budget, totalRAM());
+    if (mode != OpenMode::BULK_LOAD) {
+        return rocksdb::NewLRUCache(mem_budget / 2, 8);
+    } else {
+        // In bulk-load mode we use a lot of memory for write buffers, so
+        // provision a smaller block cache to compensate.
+        return rocksdb::NewLRUCache(mem_budget / 10, 6);
+    }
+}
+
 // Reference for RocksDB tuning: https://github.com/facebook/rocksdb/wiki/RocksDB-Tuning-Guide
-void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length,
+void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length, size_t mem_budget,
+                              std::shared_ptr<rocksdb::Cache> block_cache,
                               rocksdb::ColumnFamilyOptions& opts) {
     // universal compaction, 1GiB memtable budget
     opts.OptimizeUniversalStyleCompaction(1<<30);
@@ -100,7 +114,7 @@ void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length,
     rocksdb::BlockBasedTableOptions bbto;
     bbto.format_version = 2;
     bbto.block_size = 1024 * 1024;
-    bbto.block_cache = rocksdb::NewLRUCache(totalRAM() / 2, 8);
+    bbto.block_cache = block_cache;
 
     // compress all files with Zstandard
     opts.compression_per_level.clear();
@@ -126,11 +140,10 @@ void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length,
         // a good tradeoff for bulk loading.
         opts.memtable_factory = std::make_shared<rocksdb::VectorRepFactory>();
 
-        // Increase memtable size (and shrink block cache to compensate)
-        opts.write_buffer_size = totalRAM() / 6;
+        // Increase memtable size
+        opts.write_buffer_size = mem_budget / 6;
         opts.max_write_buffer_number = 4;
         opts.min_write_buffer_number_to_merge = 1;
-        bbto.block_cache = rocksdb::NewLRUCache(totalRAM() / 10, 6);
 
         // Never slowdown ingest since we'll wait for compaction to converge
         // at the end of the bulk load operation
@@ -146,8 +159,9 @@ void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length,
     }
 }
 
-void ApplyDBOptions(OpenMode mode, rocksdb::Options& opts) {
-    ApplyColumnFamilyOptions(mode, 0, static_cast<rocksdb::ColumnFamilyOptions&>(opts));
+void ApplyDBOptions(OpenMode mode, size_t mem_budget, size_t thread_budget,
+                    std::shared_ptr<rocksdb::Cache> block_cache, rocksdb::Options& opts) {
+    ApplyColumnFamilyOptions(mode, 0, mem_budget, block_cache, static_cast<rocksdb::ColumnFamilyOptions&>(opts));
 
     if (mode == OpenMode::READ_ONLY) {
         // override db_log_dir when we're read-only, so that the RocksDB dir
@@ -158,8 +172,12 @@ void ApplyDBOptions(OpenMode mode, rocksdb::Options& opts) {
     opts.max_open_files = -1;
 
     // configure parallelism
-    opts.max_background_jobs = std::max(std::thread::hardware_concurrency(), 2U);
-    opts.max_subcompactions = std::min(opts.max_background_jobs, 10);
+    opts.max_background_jobs = std::thread::hardware_concurrency();
+    if (thread_budget > 0 && thread_budget < opts.max_background_jobs) {
+        opts.max_background_jobs = thread_budget;
+    }
+    opts.max_background_jobs = std::max(opts.max_background_jobs, 2);
+    opts.max_subcompactions = opts.max_background_jobs;
     opts.env->SetBackgroundThreads(opts.max_background_jobs, rocksdb::Env::LOW);
     opts.env->SetBackgroundThreads(opts.max_background_jobs, rocksdb::Env::HIGH);
 
@@ -317,16 +335,18 @@ private:
     std::map<const std::string, rocksdb::ColumnFamilyHandle*> coll2handle_;
     OpenMode mode_;
     prefix_spec prefix_spec_;
+    size_t mem_budget_ = 0;
     rocksdb::WriteOptions write_options_, batch_write_options_;
+    std::shared_ptr<rocksdb::Cache> block_cache_;
 
     // No copying allowed
     DB(const DB&);
     void operator=(const DB&);
 
     DB(rocksdb::DB *db, std::map<const std::string, rocksdb::ColumnFamilyHandle*>& coll2handle,
-       OpenMode mode, prefix_spec* pfx)
+       OpenMode mode, prefix_spec* pfx, size_t mem_budget, std::shared_ptr<rocksdb::Cache> block_cache)
         : db_(db), coll2handle_(std::move(coll2handle)),
-          mode_(mode) {
+          mode_(mode), mem_budget_(mem_budget), block_cache_(block_cache) {
             if (pfx) {
                 prefix_spec_ = *pfx;
             }
@@ -340,11 +360,14 @@ private:
         }
 
 public:
-    static Status Initialize(const std::string& dbPath,
-                             std::unique_ptr<KeyValue::DB> &db,
-                             prefix_spec* pfx) {
+    static Status Initialize(const std::string& dbPath, const config& opt,
+                             std::unique_ptr<KeyValue::DB> &db) {
+        if (opt.mode != OpenMode::NORMAL && opt.mode != OpenMode::BULK_LOAD) {
+            return Status::Invalid("RocksKeyValue::Initialize: invalid open mode");
+        }
+        auto block_cache = NewBlockCache(opt.mode, opt.mem_budget);
         rocksdb::Options options;
-        ApplyDBOptions(OpenMode::NORMAL, options);
+        ApplyDBOptions(opt.mode, opt.mem_budget, opt.thread_budget, block_cache, options);
         options.create_if_missing = true;
         options.error_if_exists = true;
 
@@ -356,7 +379,7 @@ public:
         assert(rawdb != nullptr);
 
         std::map<const std::string, rocksdb::ColumnFamilyHandle*> coll2handle;
-        db.reset(new DB(rawdb, coll2handle, OpenMode::NORMAL, pfx));
+        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, opt.mem_budget, block_cache));
         if (!db) {
             delete rawdb;
             return Status::Failure();
@@ -364,13 +387,12 @@ public:
         return Status::OK();
     }
 
-    static Status Open(const std::string& dbPath,
-                       std::unique_ptr<KeyValue::DB> &db,
-                       prefix_spec *pfx,
-                       OpenMode mode) {
+    static Status Open(const std::string& dbPath, const config& opt,
+                       std::unique_ptr<KeyValue::DB> &db) {
         // prepare options
+        auto block_cache = NewBlockCache(opt.mode, opt.mem_budget);
         rocksdb::Options options;
-        ApplyDBOptions(mode, options);
+        ApplyDBOptions(opt.mode, opt.mem_budget, opt.thread_budget, block_cache, options);
         options.create_if_missing = false;
 
         // detect the database's column families
@@ -383,10 +405,10 @@ public:
         for (const auto& nm : column_family_names) {
             rocksdb::ColumnFamilyOptions colopts;
             size_t effective_pfx = 0;
-            if (pfx && nm == pfx->first) {
-                effective_pfx = pfx->second;
+            if (opt.pfx && nm == opt.pfx->first) {
+                effective_pfx = opt.pfx->second;
             }
-            ApplyColumnFamilyOptions(mode, effective_pfx, colopts);
+            ApplyColumnFamilyOptions(opt.mode, effective_pfx, opt.mem_budget, block_cache, colopts);
             rocksdb::ColumnFamilyDescriptor cfd;
             cfd.name = nm;
             cfd.options = colopts;
@@ -397,7 +419,7 @@ public:
         rocksdb::DB *rawdb = nullptr;
         std::vector<rocksdb::ColumnFamilyHandle*> column_family_handles;
 
-        if (mode == OpenMode::READ_ONLY) {
+        if (opt.mode == OpenMode::READ_ONLY) {
             s = rocksdb::DB::OpenForReadOnly(options, dbPath, column_families,
                                              &column_family_handles, &rawdb);
         } else {
@@ -414,7 +436,7 @@ public:
         for (size_t i = 0; i < column_families.size(); i++) {
             coll2handle[column_family_names[i]] = column_family_handles[i];
         }
-        db.reset(new DB(rawdb, coll2handle, mode, pfx));
+        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, opt.mem_budget, block_cache));
         if (!db) {
             for (auto h : column_family_handles) {
                 delete h;
@@ -476,7 +498,7 @@ public:
         if (name == prefix_spec_.first) {
             pfx = prefix_spec_.second;
         }
-        ApplyColumnFamilyOptions(mode_, pfx, colopts);
+        ApplyColumnFamilyOptions(mode_, pfx, mem_budget_, block_cache_, colopts);
         rocksdb::ColumnFamilyHandle *handle;
         rocksdb::Status s = db_->CreateColumnFamily(colopts, name, &handle);
         if (!s.ok()) {
@@ -531,15 +553,14 @@ public:
     }
 };
 
-Status Initialize(const std::string& dbPath, std::unique_ptr<KeyValue::DB>& db, prefix_spec* pfx)
+Status Initialize(const std::string& dbPath, const config& opt, std::unique_ptr<KeyValue::DB>& db)
 {
-    return DB::Initialize(dbPath, db, pfx);
+    return DB::Initialize(dbPath, opt, db);
 }
 
-Status Open(const std::string& dbPath, std::unique_ptr<KeyValue::DB>& db,
-            prefix_spec* pfx, OpenMode mode)
+Status Open(const std::string& dbPath, const config& opt, std::unique_ptr<KeyValue::DB>& db)
 {
-    return DB::Open(dbPath, db, pfx, mode);
+    return DB::Open(dbPath, opt, db);
 }
 
 Status destroy(const std::string dbPath)

src/cli_utils.cc
@@ -820,8 +820,10 @@ Status db_init(std::shared_ptr<spdlog::logger> logger,
     free(contignames);
 
     // create and initialize the database
+    RocksKeyValue::config cfg;
+    cfg.pfx = GLnexus_prefix_spec();
     unique_ptr<KeyValue::DB> db;
-    S(RocksKeyValue::Initialize(dbpath, db, GLnexus_prefix_spec()));
+    S(RocksKeyValue::Initialize(dbpath, cfg, db));
     S(BCFKeyValueData::InitializeDB(db.get(), contigs, bucket_size));
 
     // report success
@@ -847,10 +849,11 @@ Status db_get_contigs(std::shared_ptr<spdlog::logger> logger,
     Status s;
     logger->info("db_get_contigs {}", dbpath);
 
+    RocksKeyValue::config cfg;
+    cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
+    cfg.pfx = GLnexus_prefix_spec();
     unique_ptr<KeyValue::DB> db;
-
-    S(RocksKeyValue::Open(dbpath, db, GLnexus_prefix_spec(),
-                          RocksKeyValue::OpenMode::READ_ONLY));
+    S(RocksKeyValue::Open(dbpath, cfg, db));
     {
         unique_ptr<BCFKeyValueData> data;
         S(BCFKeyValueData::Open(db.get(), data));
@@ -861,7 +864,7 @@ Status db_get_contigs(std::shared_ptr<spdlog::logger> logger,
 }
 
 Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
-                    size_t nr_threads,
+                    size_t mem_budget, size_t nr_threads,
                     const vector<string> &gvcfs,
                     const string &dbpath,
                     const vector<range> &ranges_i,
@@ -874,9 +877,13 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
         ranges.insert(r);
 
     // open the database
+    RocksKeyValue::config cfg;
+    cfg.mode = RocksKeyValue::OpenMode::BULK_LOAD;
+    cfg.pfx = GLnexus_prefix_spec();
+    cfg.mem_budget = mem_budget;
+    cfg.thread_budget = nr_threads/2;
     unique_ptr<KeyValue::DB> db;
-    S(RocksKeyValue::Open(dbpath, db, GLnexus_prefix_spec(),
-                          RocksKeyValue::OpenMode::BULK_LOAD));
+    S(RocksKeyValue::Open(dbpath, cfg, db));
     unique_ptr<BCFKeyValueData> data;
     S(BCFKeyValueData::Open(db.get(), data));
 
@@ -894,7 +901,7 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
         logger->info("Beginning bulk load with no range filter.");
     }
 
-    ctpl::thread_pool threadpool(nr_threads);
+    ctpl::thread_pool threadpool(nr_threads || std::thread::hardware_concurrency());
     vector<future<Status>> statuses;
     set<string> datasets_loaded;
     BCFKeyValueData::import_result stats;
@@ -982,7 +989,7 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
 }
 
 Status discover_alleles(std::shared_ptr<spdlog::logger> logger,
-                        size_t nr_threads,
+                        size_t mem_budget, size_t nr_threads,
                         const string &dbpath,
                         const vector<range> &ranges,
                         const std::vector<std::pair<std::string,size_t> > &contigs,
@@ -994,13 +1001,17 @@ Status discover_alleles(std::shared_ptr<spdlog::logger> logger,
     dsals.clear();
 
     // open the database in read-only mode
-    S(RocksKeyValue::Open(dbpath, db, GLnexus_prefix_spec(),
-                          RocksKeyValue::OpenMode::READ_ONLY));
+    RocksKeyValue::config cfg;
+    cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
+    cfg.pfx = GLnexus_prefix_spec();
+    cfg.mem_budget = mem_budget;
+    cfg.thread_budget = nr_threads;
+    S(RocksKeyValue::Open(dbpath, cfg, db));
     S(BCFKeyValueData::Open(db.get(), data));
 
     // start service, discover alleles
     service_config svccfg;
-    svccfg.threads = nr_threads;
+    svccfg.threads = nr_threads || std::thread::hardware_concurrency();
     unique_ptr<Service> svc;
     S(Service::Start(svccfg, *data, *data, svc));
 
@@ -1047,7 +1058,7 @@ Status unify_sites(std::shared_ptr<spdlog::logger> logger,
 
 
 Status genotype(std::shared_ptr<spdlog::logger> logger,
-                size_t nr_threads,
+                size_t mem_budget, size_t nr_threads,
                 const string &dbpath,
                 const genotyper_config &genotyper_cfg,
                 const vector<unified_site> &sites,
@@ -1057,9 +1068,13 @@ Status genotype(std::shared_ptr<spdlog::logger> logger,
     logger->info("Lifting over {} fields", genotyper_cfg.liftover_fields.size());
 
     // open the database in read-only mode
+    RocksKeyValue::config cfg;
+    cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
+    cfg.pfx = GLnexus_prefix_spec();
+    cfg.mem_budget = mem_budget;
+    cfg.thread_budget = nr_threads;
     unique_ptr<KeyValue::DB> db;
-    S(RocksKeyValue::Open(dbpath, db, GLnexus_prefix_spec(),
-                                   RocksKeyValue::OpenMode::READ_ONLY));
+    S(RocksKeyValue::Open(dbpath, cfg, db));
     unique_ptr<BCFKeyValueData> data;
     S(BCFKeyValueData::Open(db.get(), data));
 
@@ -1068,7 +1083,7 @@ Status genotype(std::shared_ptr<spdlog::logger> logger,
 
     // start service, discover alleles, unify sites, genotype sites
     service_config svccfg;
-    svccfg.threads = nr_threads;
+    svccfg.threads = nr_threads || std::thread::hardware_concurrency();
     svccfg.extra_header_lines = extra_header_lines;
     unique_ptr<Service> svc;
     S(Service::Start(svccfg, *data, *data, svc));
@@ -1095,16 +1110,20 @@ Status compare_db_itertion_algorithms(std::shared_ptr<spdlog::logger> logger,
                                       const std::string &dbpath,
                                       int n_iter) {
     Status s;
+
     unique_ptr<KeyValue::DB> db;
+    RocksKeyValue::config cfg;
+    cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
+    cfg.pfx = GLnexus_prefix_spec();
+    S(RocksKeyValue::Open(dbpath, cfg, db));
+
     unique_ptr<BCFKeyValueData> data;
-    string sampleset;
-    S(RocksKeyValue::Open(dbpath, db, GLnexus_prefix_spec(),
-                          RocksKeyValue::OpenMode::READ_ONLY));
     S(BCFKeyValueData::Open(db.get(), data));
 
     unique_ptr<MetadataCache> metadata;
     S(MetadataCache::Start(*data, metadata));
 
+    string sampleset;
     S(data->all_samples_sampleset(sampleset));
     logger->info("using sample set {}", sampleset);
 

test/cli_utils.cc
@@ -559,7 +559,7 @@ TEST_CASE("iter_compare") {
          gvcfs.push_back(basedir + "/" + fname);
     }
     vector<range> ranges;
-    s = cli::utils::db_bulk_load(console, 8, gvcfs, dbpath, ranges, contigs);
+    s = cli::utils::db_bulk_load(console,  0, 8, gvcfs, dbpath, ranges, contigs);
     REQUIRE(s.ok());
     REQUIRE(contigs.size() >= 1);
 

cli/glnexus_cli.cc
@@ -39,7 +39,7 @@ GLnexus::Status s;
 static int all_steps(const vector<string> &vcf_files,
                      const string &bedfilename,
                      const string &config_name,
-                     int nr_threads,
+                     size_t mem_budget, size_t nr_threads,
                      bool debug,
                      bool iter_compare,
                      size_t bucket_size) {
@@ -76,7 +76,7 @@ static int all_steps(const vector<string> &vcf_files,
         // use an empty range filter
         vector<GLnexus::range> ranges;
         H("bulk load into DB",
-          GLnexus::cli::utils::db_bulk_load(console, nr_threads, vcf_files, dbpath, ranges, contigs, false));
+          GLnexus::cli::utils::db_bulk_load(console, mem_budget, nr_threads, vcf_files, dbpath, ranges, contigs, false));
     }
 
     if (iter_compare) {
@@ -92,7 +92,7 @@ static int all_steps(const vector<string> &vcf_files,
     GLnexus::discovered_alleles dsals;
     unsigned sample_count = 0;
     H("discover alleles",
-      GLnexus::cli::utils::discover_alleles(console, nr_threads, dbpath, ranges, contigs, dsals, sample_count));
+      GLnexus::cli::utils::discover_alleles(console, mem_budget, nr_threads, dbpath, ranges, contigs, dsals, sample_count));
     if (debug) {
         string filename("/tmp/dsals.yml");
         console->info("Writing discovered alleles as YAML to {}", filename);
@@ -139,7 +139,7 @@ static int all_steps(const vector<string> &vcf_files,
     }
     string outfile("-");
     H("Genotyping",
-      GLnexus::cli::utils::genotype(console, nr_threads, dbpath, genotyper_cfg, sites, hdr_lines, outfile));
+      GLnexus::cli::utils::genotype(console, mem_budget, nr_threads, dbpath, genotyper_cfg, sites, hdr_lines, outfile));
 
     return 0;
 }
@@ -150,10 +150,12 @@ void help(const char* prog) {
          << "Merge and joint-call input gVCF files, emitting multi-sample BCF on" << endl
          << "standard output." << endl << endl
          << "Options:" << endl
-         << "  --help, -h           print this help message" << endl
-         << "  --bed FILE, -b FILE  three-column BED file of ranges to analyze (required)" << endl
-         << "  --config X, -c X     configuration preset name or .yml filename (default: gatk)" << endl
-         << "  --list, -l           given files contain lists of gVCF filenames, one per line" << endl
+         << "  --bed FILE, -b FILE   three-column BED file of ranges to analyze (required)" << endl
+         << "  --config X, -c X      configuration preset name or .yml filename (default: gatk)" << endl
+         << "  --list, -l            given files contain lists of gVCF filenames, one per line" << endl
+         << "  --mem-gbytes X, -m X  memory budget, in gbytes (default: most of system memory)" << endl
+         << "  --threads X, -t X     thread budget (default: all hardware threads)" << endl
+         << "  --help, -h            print this help message" << endl
          << endl << "Configuration presets:" << endl;
     cout << GLnexus::cli::utils::describe_config_presets() << endl;
 }
@@ -178,6 +180,8 @@ int main(int argc, char *argv[]) {
         {"bed", required_argument, 0, 'b'},
         {"config", required_argument, 0, 'c'},
         {"list", no_argument, 0, 'l'},
+        {"mem-gbytes", required_argument, 0, 'm'},
+        {"threads", required_argument, 0, 't'},
         {"bucket_size", required_argument, 0, 'x'},
         {"debug", no_argument, 0, 'd'},
         {"iter_compare", no_argument, 0, 'i'},
@@ -190,10 +194,10 @@ int main(int argc, char *argv[]) {
     bool debug = false;
     bool iter_compare = false;
     string bedfilename;
-    int nr_threads = std::thread::hardware_concurrency();
+    size_t mem_budget = 0, nr_threads = 0;
     size_t bucket_size = GLnexus::BCFKeyValueData::default_bucket_size;
 
-    while (-1 != (c = getopt_long(argc, argv, "hb:dIx:",
+    while (-1 != (c = getopt_long(argc, argv, "hb:dIx:m:t:",
                                   long_options, nullptr))) {
         switch (c) {
             case 'b':
@@ -234,6 +238,23 @@ int main(int argc, char *argv[]) {
                 }
                 break;
 
+            case 'm':
+                mem_budget = strtoull(optarg, nullptr, 10);
+                if (mem_budget == 0 || mem_budget > 16*1024) {
+                    cerr << "invalid --mem-gbytes" << endl;
+                    return 1;
+                }
+                mem_budget <<= 30;
+                break;
+
+            case 't':
+                nr_threads = strtoull(optarg, nullptr, 10);
+                if (nr_threads == 0 || nr_threads > 1024) {
+                    cerr << "invalid --threads" << endl;
+                    return 1;
+                }
+                break;
+
             default:
                 abort ();
         }
@@ -264,5 +285,5 @@ int main(int argc, char *argv[]) {
         vcf_files = vcf_files_precursor;
     }
 
-    return all_steps(vcf_files, bedfilename, config_name, nr_threads, debug, iter_compare, bucket_size);
+    return all_steps(vcf_files, bedfilename, config_name, mem_budget, nr_threads, debug, iter_compare, bucket_size);
 }

include/RocksKeyValue.h
@@ -17,11 +17,6 @@ namespace RocksKeyValue {
 /// [prefix_spec].
 using prefix_spec = std::pair<std::string, size_t>;
 
-/// Initialize a new database. The parent directory must exist. Fails if the
-/// path already exists.
-Status Initialize(const std::string& dbpath, std::unique_ptr<KeyValue::DB>& db,
-                  prefix_spec* pfx = nullptr);
-
 /// Database open mode
 enum class OpenMode {
     /// Online, read-write operations
@@ -38,9 +33,19 @@ enum class OpenMode {
     BULK_LOAD
 };
 
+struct config {
+    prefix_spec *pfx = nullptr;
+    OpenMode mode = OpenMode::NORMAL;
+    size_t mem_budget = 0;
+    size_t thread_budget = 0;
+};
+
+/// Initialize a new database. The parent directory must exist. Fails if the
+/// path already exists.
+Status Initialize(const std::string& dbpath, const config& cfg, std::unique_ptr<KeyValue::DB>& db);
+
 /// Open an existing database.
-Status Open(const std::string& dbPath, std::unique_ptr<KeyValue::DB>& db,
-            prefix_spec* pfx = nullptr, OpenMode mode=OpenMode::NORMAL);
+Status Open(const std::string& dbPath, const config& cfg, std::unique_ptr<KeyValue::DB>& db);
 
 // Delete an existing database.
 Status destroy(const std::string dbPath);

commit fffd6b7729ea4b745e8cb15e24161e384505d7e6

Fix performance regression during gVCF bulk load introduced in #134
The thread and memory budgets were not taking the desired default values when the user did not specify them on the command-line.

src/RocksKeyValue.cc
@@ -46,6 +46,17 @@ static size_t totalRAM() {
     return memoized;
 }
 
+// Given a user-specified memory budget (zero if none), calculate the practical
+// effective memory budget
+size_t calculate_mem_budget(size_t specified_mem_budget) {
+    size_t ans = totalRAM();
+    if (specified_mem_budget > 0) {
+        ans = std::min(ans, specified_mem_budget);
+    }
+    ans = std::max(ans, 4U*size_t(1<<30));
+    return ans;
+}
+
 // Convert a rocksdb Status into a GLnexus Status structure
 static Status convertStatus(const rocksdb::Status &s)
 {
@@ -83,8 +94,7 @@ static Status convertStatus(const rocksdb::Status &s)
 
 // Create RocksDB block cache to be shared among all collections in one database
 std::shared_ptr<rocksdb::Cache> NewBlockCache(OpenMode mode, size_t mem_budget) {
-    mem_budget = std::max(mem_budget, size_t(2<<30));
-    mem_budget = std::min(mem_budget, totalRAM());
+    assert(mem_budget >= 4U*size_t(1<<30));
     if (mode != OpenMode::BULK_LOAD) {
         return rocksdb::NewLRUCache(mem_budget / 2, 8);
     } else {
@@ -141,6 +151,7 @@ void ApplyColumnFamilyOptions(OpenMode mode, size_t prefix_length, size_t mem_bu
         opts.memtable_factory = std::make_shared<rocksdb::VectorRepFactory>();
 
         // Increase memtable size
+        assert(mem_budget >= 4U*size_t(1<<30));
         opts.write_buffer_size = mem_budget / 6;
         opts.max_write_buffer_number = 4;
         opts.min_write_buffer_number_to_merge = 1;
@@ -365,9 +376,10 @@ public:
         if (opt.mode != OpenMode::NORMAL && opt.mode != OpenMode::BULK_LOAD) {
             return Status::Invalid("RocksKeyValue::Initialize: invalid open mode");
         }
-        auto block_cache = NewBlockCache(opt.mode, opt.mem_budget);
+        size_t mem_budget = calculate_mem_budget(opt.mem_budget);
+        auto block_cache = NewBlockCache(opt.mode, mem_budget);
         rocksdb::Options options;
-        ApplyDBOptions(opt.mode, opt.mem_budget, opt.thread_budget, block_cache, options);
+        ApplyDBOptions(opt.mode, mem_budget, opt.thread_budget, block_cache, options);
         options.create_if_missing = true;
         options.error_if_exists = true;
 
@@ -379,7 +391,7 @@ public:
         assert(rawdb != nullptr);
 
         std::map<const std::string, rocksdb::ColumnFamilyHandle*> coll2handle;
-        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, opt.mem_budget, block_cache));
+        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, mem_budget, block_cache));
         if (!db) {
             delete rawdb;
             return Status::Failure();
@@ -390,9 +402,10 @@ public:
     static Status Open(const std::string& dbPath, const config& opt,
                        std::unique_ptr<KeyValue::DB> &db) {
         // prepare options
-        auto block_cache = NewBlockCache(opt.mode, opt.mem_budget);
+        size_t mem_budget = calculate_mem_budget(opt.mem_budget);
+        auto block_cache = NewBlockCache(opt.mode, mem_budget);
         rocksdb::Options options;
-        ApplyDBOptions(opt.mode, opt.mem_budget, opt.thread_budget, block_cache, options);
+        ApplyDBOptions(opt.mode, mem_budget, opt.thread_budget, block_cache, options);
         options.create_if_missing = false;
 
         // detect the database's column families
@@ -408,7 +421,7 @@ public:
             if (opt.pfx && nm == opt.pfx->first) {
                 effective_pfx = opt.pfx->second;
             }
-            ApplyColumnFamilyOptions(opt.mode, effective_pfx, opt.mem_budget, block_cache, colopts);
+            ApplyColumnFamilyOptions(opt.mode, effective_pfx, mem_budget, block_cache, colopts);
             rocksdb::ColumnFamilyDescriptor cfd;
             cfd.name = nm;
             cfd.options = colopts;
@@ -436,7 +449,7 @@ public:
         for (size_t i = 0; i < column_families.size(); i++) {
             coll2handle[column_family_names[i]] = column_family_handles[i];
         }
-        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, opt.mem_budget, block_cache));
+        db.reset(new DB(rawdb, coll2handle, opt.mode, opt.pfx, mem_budget, block_cache));
         if (!db) {
             for (auto h : column_family_handles) {
                 delete h;

src/cli_utils.cc
@@ -969,6 +969,10 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
                     bool delete_gvcf_after_load) {
     Status s;
 
+    if (nr_threads == 0) {
+        nr_threads = std::thread::hardware_concurrency();
+    }
+
     set<range> ranges;
     for (auto &r : ranges_i)
         ranges.insert(r);
@@ -978,7 +982,7 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
     cfg.mode = RocksKeyValue::OpenMode::BULK_LOAD;
     cfg.pfx = GLnexus_prefix_spec();
     cfg.mem_budget = mem_budget;
-    cfg.thread_budget = nr_threads/2;
+    cfg.thread_budget = nr_threads;
     unique_ptr<KeyValue::DB> db;
     S(RocksKeyValue::Open(dbpath, cfg, db));
     unique_ptr<BCFKeyValueData> data;
@@ -998,7 +1002,7 @@ Status db_bulk_load(std::shared_ptr<spdlog::logger> logger,
         logger->info("Beginning bulk load with no range filter.");
     }
 
-    ctpl::thread_pool threadpool(nr_threads || std::thread::hardware_concurrency());
+    ctpl::thread_pool threadpool(nr_threads);
     vector<future<Status>> statuses;
     set<string> datasets_loaded;
     BCFKeyValueData::import_result stats;
@@ -1097,6 +1101,10 @@ Status discover_alleles(std::shared_ptr<spdlog::logger> logger,
     unique_ptr<BCFKeyValueData> data;
     dsals.clear();
 
+    if (nr_threads == 0) {
+        nr_threads = std::thread::hardware_concurrency();
+    }
+
     // open the database in read-only mode
     RocksKeyValue::config cfg;
     cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
@@ -1108,7 +1116,7 @@ Status discover_alleles(std::shared_ptr<spdlog::logger> logger,
 
     // start service, discover alleles
     service_config svccfg;
-    svccfg.threads = nr_threads || std::thread::hardware_concurrency();
+    svccfg.threads = nr_threads;
     unique_ptr<Service> svc;
     S(Service::Start(svccfg, *data, *data, svc));
 
@@ -1164,6 +1172,10 @@ Status genotype(std::shared_ptr<spdlog::logger> logger,
     Status s;
     logger->info("Lifting over {} fields", genotyper_cfg.liftover_fields.size());
 
+    if (nr_threads == 0) {
+        nr_threads = std::thread::hardware_concurrency();
+    }
+
     // open the database in read-only mode
     RocksKeyValue::config cfg;
     cfg.mode = RocksKeyValue::OpenMode::READ_ONLY;
@@ -1180,7 +1192,7 @@ Status genotype(std::shared_ptr<spdlog::logger> logger,
 
     // start service, discover alleles, unify sites, genotype sites
     service_config svccfg;
-    svccfg.threads = nr_threads || std::thread::hardware_concurrency();
+    svccfg.threads = nr_threads;
     svccfg.extra_header_lines = extra_header_lines;
     unique_ptr<Service> svc;
     S(Service::Start(svccfg, *data, *data, svc));